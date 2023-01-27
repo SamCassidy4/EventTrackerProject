49528b0f16fc30f2f12f7fa5b10bfa7c879e7c98
Sam Cassidy <samuelcassidy1999@gmail.com>
2023-01-27 16:03:50 -0700

    Able to see workout names on webpage, stubbed out a create workout form and XMLHTTPRequest

--- ExerciseEveryday/src/main/resources/static/js/script.js
+++ ExerciseEveryday/src/main/resources/static/js/script.js
@@ -22,7 +22,7 @@ function loadWorkouts(){
 				displayWorkouts(list);
 			}
 			else{
-				//TODO - display error
+				displayError('Workouts not Found')
 			}
 		}
 	};
@@ -30,6 +30,47 @@ function loadWorkouts(){
 	xhr.send();
 }
 
-function displayWorkouts(workoutList){
+function addWorkout(newWorkout){
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/workouts');
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4){
+			if(xhr.status === 200 || xhr.status === 201){
+				let workout = JSON.parse(xhr.responseText);
+				displaySingleWorkout(workout);
+			} 
+			else{
+				displayError("Error in Creating Workout: " + xhr.status);
+			}
+		}
+	};
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify(newWorkout));
+}
+
+function displayError(message){
+	let div = document.getElementById('eventList');
+	div.textContent = message;
+}
+//TODO: build display SingleWorkout using workout id
+function displaySingleWorkout(workout){
+	
+}
+
+function displayWorkouts(workout){
+	console.log(workout);	
+	let dataDiv = document.getElementById('eventList');
+	dataDiv.textContent = '';
+	
+	let ul = document.createElement('ul');
+	workout.forEach((workout)=>{
+		console.log(workout);
+		
+		let li = document.createElement('li');
+		li.textContent = workout.name
+		ul.appendChild(li);
+	});
+	dataDiv.appendChild(ul)
+	
 	//DOM
 }
